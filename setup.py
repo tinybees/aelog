c74bfc5ec654d2454b0c2ca8bda4aa1b1f0c9412
guoyanfeng <guoyanfeng@rongcapital.cn>
2018-03-25 20:52:52 +0800

    增加readme帮助文档
    Signed-off-by: guoyanfeng <guoyanfeng@rongcapital.cn>

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='aelog',
+    version='1.0.0',
+    packages=['aelog'],
+    url='',
+    license='',
+    author='guoyanfeng',
+    author_email='',
+    description=''
+)
---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+@author: guoyanfeng
+@software: PyCharm
+@time: 18-3-25 下午7:54
+"""
+
